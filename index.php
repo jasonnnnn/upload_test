07c29ed8a65d8ebe3c98a86186de445057a8f3cc
Yonghwi Lee <yonghwi.lee@43492-205.auckland.natcoll.ac.nz>
2015-06-09 16:55:38 +1200

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php # index.php
+
+
+	#1 Load libraries
+
+	require_once 'form.lib.php';
+	require_once 'upload.lib.php';
+	require_once 'url.lib.php';
+
+
+	#2 Logic
+
+	// print_r($_FILES);
+
+	if($_FILES){
+
+		$tmp      = $_FILES['file']['tmp_name'][0];
+		$filename = $_FILES['file']['name'][0];
+
+		
+		move_uploaded_file($tmp, 'uploads/'.$filename);
+
+
+		URL::redirect('uploads/'.$filename);
+	}
+
+
+
+
+	#3 load views (is after this php tag)
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Upload files with PHP</title>
+</head>
+<body>
+
+	<h1>Upload files with PHP</h1>
+
+	<?= Form::open_upload() ?>
+
+	<div class="form-group">
+		
+		<?= Form::label('file', 'File:') ?>
+		<?= Form::file() ?>
+	</div>
+
+	<?= Form::submit() ?>
+
+	<?= Form::close() ?>
+	
+</body>
+</html>
